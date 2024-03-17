93bb163fa52e13fdffe6262f4ee4b1bf9c37b368
Gaston Ranz <gastonranz94@gmail.com>
2024-03-17 18:36:36 -0300

    New Update March 17th, Sunday 6:36 pm

--- src/Class5and7/Main2.java
+++ src/Class5and7/Main2.java
@@ -1,12 +1,5 @@
 package Class5and7;
 
-public class Main2 {
-    public static void main(String[] args) {
-        Double num = 20d;
-        Double num2 = 20d;
-        double num3 = 20d;
-
-        if(num != null && num2 != null && num == num2) System.out.println(num.equals(num3));
-        System.out.println(num == num3);
-    }
+public abstract class Main2 {
+    public abstract String getName();
 }
